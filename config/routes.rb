f03bf1d8c90e6b211d5d1eec64f4ff4277fe13f7
Kerry Mui <kerry.t.mui@gmail.com>
2014-04-20 19:05:06 -0400

    Creating a user model, controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Seatyourself2::Application.routes.draw do
+  get "users/new"
+  get "users/create"
   resources :restaurants
+  resources :users, :only => [:new, :create]
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140420213237) do
+ActiveRecord::Schema.define(version: 20140420224053) do
 
   create_table "reservations", force: true do |t|
     t.integer  "restaurant_id"
@@ -29,6 +29,14 @@ ActiveRecord::Schema.define(version: 20140420213237) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "capacity"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "email"
+    t.string   "name"
+    t.string   "password_digest"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
