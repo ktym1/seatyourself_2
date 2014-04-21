715c9719e137eaf4eed7622b48fa23972406dbef
Kerry Mui <kerry.t.mui@gmail.com>
2014-04-21 16:43:34 -0400

    added user_id to reservation table, completed reservations_controller, updated restaurants_controller, began reservation form in restaurant.show.html.erb

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,16 +1,37 @@
 class ReservationsController < ApplicationController
-  def show
-  end
+  before_filter :load_restaurant
 
-  def new
+  def show
+    @reservation = Reservation.find(params[:id])
   end
 
   def create
+    @reservation = @restaurant.reservation.build(reservation_params)
+    @reservation.user_id = current_user.id
+
+    if @reservation.save
+      redirect_to restaurants_path, notice: "Reservation created successfully"
+
+    else
+      render :action => :show
+    end
   end
 
-  def edit
-  end
+  # def edit
+  # end
 
   def destroy
+      @reservation = Reservation.find(params[:id])
+      @reservation.destroy
   end
+
+private
+  def reservation_params
+    params.require(:reservation).permit(:restaurant_id, :party_size, :party_time)
+  end
+
+  def load_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140420224053) do
+ActiveRecord::Schema.define(version: 20140421203957) do
 
   create_table "reservations", force: true do |t|
     t.integer  "restaurant_id"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20140420224053) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.datetime "party_time"
+    t.integer  "user_id"
   end
 
   create_table "restaurants", force: true do |t|
