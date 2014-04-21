e6df2faf467eb2c0b4ed8c7d9757d22e88e5a24d
Kerry Mui <kerry.t.mui@gmail.com>
2014-04-21 16:11:02 -0400

    Created reservations as a nested resource of restaurant. Created reservations controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,20 @@
 Seatyourself2::Application.routes.draw do
-  get "sessions/new"
-  get "sessions/create"
-  get "sessions/destroy"
-  get "users/new"
-  get "users/create"
-  resources :restaurants
+  # get "reservations/show"
+  # get "reservations/new"
+  # get "reservations/create"
+  # get "reservations/edit"
+  # get "reservations/destroy"
+  # # get "sessions/new"
+  # get "sessions/create"
+  # get "sessions/destroy"
+  # get "users/new"
+  # get "users/create"
+  resources :restaurants do
+  	resources :reservations, :except => [:index]
+
+  end
+
+
   resources :users, :only => [:new, :create]
   resources :sessions, :only => [:new, :create, :destroy]
 end
