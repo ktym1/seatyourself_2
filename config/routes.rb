716ba786c177632e98235af014e6f8837827c0e0
Kerry Mui <kerry.t.mui@gmail.com>
2014-04-21 15:11:29 -0400

    Creating log in functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Seatyourself2::Application.routes.draw do
+  get "sessions/new"
+  get "sessions/create"
+  get "sessions/destroy"
   get "users/new"
   get "users/create"
   resources :restaurants
   resources :users, :only => [:new, :create]
-
+  resources :sessions, :only => [:new, :create, :destroy]
 end
