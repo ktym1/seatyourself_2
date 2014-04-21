0b4100fda4245cee84099aba1cf41186d99346f4
Kerry Mui <kerry.t.mui@gmail.com>
2014-04-21 15:57:53 -0400

    Added model associations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,6 @@
 class Restaurant < ActiveRecord::Base
 	has_many :reservations
+	has_many :users, :through => :reservations
 
 	validates :name, :address, :phone, :capacity, :presence => true
 	def availability(party_size)
